2570befcb1eb0dc418f00bde079dfae5dfd7aa5c
Arianna Cabebe <aricabebe@gmail.com>
2022-02-03 14:44:22 -0800

    adds watering plant interactions and Plant tags

--- Assets/Collisions.cs
+++ Assets/Collisions.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
+    public GameObject target;
+
     // Start is called before the first frame update
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        Debug.Log(other.gameObject);
+    void OnTriggerEnter2D(Collider2D other) {
+        target = other.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        target = null;
+    }
+    void Update() {           
+        if (Input.GetKeyDown("space"))
+        {   if (!target)
+            {
+                return;
+            }
+
+            if (target.tag == "Plant") 
+            {
+                Debug.Log("I'm watering Plant!");
+            }
+        }
     }
 
 }
